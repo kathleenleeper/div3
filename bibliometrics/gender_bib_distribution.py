f258ff713292ca6d113721c9fc457c9b93d7e040
Kathleen Leeper <kathleen.leeper@gmail.com>
2015-02-17 16:09:56 -0500

    clean parsing file

--- bibliometrics/gender_bib_distribution.py
+++ bibliometrics/gender_bib_distribution.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[135]:
+
 from __future__ import division
 from genderComputer import GenderComputer #import gendercomputer - more fully featured
 import pprint as pp
@@ -6,41 +11,59 @@ import bibtexparser as b #module for bibtexin'
 from bibtexparser.bparser import BibTexParser #import to add customization
 from bibtexparser.customization import *
 
-""""""""""""""""""""""
-initializing variables 
-"""""""""""""""""""""
+
+# In[136]:
+
 gc = GenderComputer(os.path.abspath('./nameLists')) #make gendercomputer
-women = 0
-men = 0
-uni = 0
-notav = 0
-auCount = 0
+
+
+# In[137]:
+
 bib = 'CriticalOpenNeuro.bib' #bring that bib file in
+
+
+# In[138]:
 
 def customizations(record):
     """Use some functions delivered by the library
-    
+
     :param record: a record
     :returns: -- customized record
     """
     record = type(record)
     record = doi(record)
-    record = convert_to_unicode(record) #clean up for parsing
-    record = author(record) #parse authors into array
+    record = convert_to_unicode(record)
+    record = author(record)
     return record
 
-def loadBibFile():
-    with open(bib) as bibtex_file:
-        parser = BibTexParser()
-        parser.homogenize = True
-        parser.customization = customizations
-        data = b.load(bibtex_file, parser = parser)
 
+# In[139]:
+
+with open(bib) as bibtex_file: 
+    parser = BibTexParser()
+    parser.homogenize = True
+    parser.customization = customizations
+    data = b.load(bibtex_file, parser = parser)
+
+
+# In[144]:
+
+"""
+initialize variables 
+"""
+women = 0
+men = 0
+uni = 0
+notav = 0
+auCount = 0
+
+
+# In[145]:
 
 for entry in data.entries:
     title = entry["title"]
     if "author" in entry:
-        authors = entry["author"]
+        authors = entry["author"] 
     else:
         print "no author in", title
     for j in authors:
@@ -52,12 +75,12 @@ for entry in data.entries:
             women += 1
         elif gender == 'unisex':
             uni += 1
-        else: 
-            #print j, title
-            notav += 1 
+        else:
+            notav += 1
 
-#'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
-#calculate percents #hacks
+
+# In[146]:
+
 print "authors ungendered:", notav
 percentMen = men/auCount*100
 percentWomen = women/auCount*100
@@ -68,4 +91,9 @@ print 'women:', women,",", "%.2f" % percentWomen, '%'
 print 'men:', men,",", "%.2f" %  percentMen, '%'
 print 'unisex:', uni,",", "%.2f" %  percentUni, '%'
 print 'unassigned:', notav,",", "%.2f" %  percentNotAv, '%'
+
+
+# In[ ]:
+
+
 
